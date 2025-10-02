cdb928b6f9bd4f133d822a3a920c96755c5bf4ec
Gu2tavo12 <gustavo.retana@catolica.edu.sv>
2025-10-01 19:25:00 -0600

    rutas de comentarios y calificaciones

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import rolesRoutes from "./routes/roles.routes.js";
 import usuariosRoutes from "./routes/usuarios.routes.js";
 import publicacionesRoutes from "./routes/publicaciones.routes.js";
+import comentariosRoutes from "./routes/comentarios.routes.js"
+import calificacionesRoutes from "./routes/calificaciones.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 dotenv.config();
@@ -19,6 +21,8 @@ app.get("/", (req, res) => {
 app.use("/api/roles", rolesRoutes);
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/publicaciones", publicacionesRoutes);
+app.use("/api/comentarios", comentariosRoutes);
+app.use("/api/calificaciones", calificacionesRoutes);
 
 app.use(errorHandler);
 
